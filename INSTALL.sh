dd36b92ed9e37ba49bd95654d899acd2f500e0bd
Ryosuke Ito <rito.0305@gmail.com>
2015-04-06 01:40:59 +0900

    FIX: correct URL

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,7 +4,7 @@ if type git rcup; then
     git clone --branch master --single-branch --depth 1 --recursive https://github.com/manicmaniac/dotfiles
     env RCRC=dotfiles/rcrc rcup
 else
-    echo 'Dependencies unsatisfied. read "https://raw.githubusercontent.com/manicmaniac/dotfiles/master/README.md#install".' >&2
+    echo 'Dependencies unsatisfied. read "https://github.com/manicmaniac/dotfiles#install".' >&2
     exit 1
 fi
 
