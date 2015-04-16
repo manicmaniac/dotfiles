869cb2ba4348013193d31d1d595ceb652d79586f
Ryosuke Ito <rito.0305@gmail.com>
2015-04-16 23:01:46 +0900

    CHANGE: Retab C templates

--- vim/template/template.m
+++ vim/template/template.m
@@ -1,10 +1,8 @@
-:call cursor(5, 3)
+:call cursor(3, 3)
 #import <Foundation/Foundation.h>
 
-int main(int argc, char const* argv[])
-{
-	
-	
-	return 0;
+int main(int argc, char const* argv[]) {
+    
+    return 0;
 }
 
